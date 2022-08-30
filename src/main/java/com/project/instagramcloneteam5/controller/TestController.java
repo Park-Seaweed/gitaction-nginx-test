4d9e4c1636f4c1be3e850dc5809a810eae50f81e
alsgur <qkralsgur721@gmail.com>
2022-08-30 11:28:05 +0900

    테스트 컨트롤러 작성

--- src/main/java/com/project/instagramcloneteam5/controller/TestController.java
+++ src/main/java/com/project/instagramcloneteam5/controller/TestController.java
@@ -1,14 +1,22 @@
 package com.project.instagramcloneteam5.controller;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/test")
-    public String test(){
-        String tesStr = "test check";
-        System.out.println(tesStr);
-        return tesStr;
+    @Value("${logging.module.version}")
+    private String version;
+
+    @GetMapping("/")
+    public String version() {
+        return String.format("Project Version : %s", version);
+    }
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
     }
 }
