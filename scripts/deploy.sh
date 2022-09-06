306078b638f70ad32d54bb680e037b03c4016c5f
alsgur <qkralsgur721@gmail.com>
2022-09-07 01:51:30 +0900

    테스트 컨트롤러 작성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,39 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-REPOSITORY=/home/ec2-user/step
-PROJECT_NAME=test_deploy
+REPOSITORY=/home/ec2-user/test-deploy
+cd $REPOSITORY
 
-echo "> Build 파일 복사"
+APP_NAME=gitaction-nginx-test
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(lsof-ti tcp:8080)
-
-echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
 else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
+  sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-
-echo "> JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-nohup java -jar \
-  -Dspring.config.location=classpath:/application.yml \
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+echo "> $JAR_PATH 배포"
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
