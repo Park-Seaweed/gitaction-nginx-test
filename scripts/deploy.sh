bce9d7a4651e5371a3f638ab97db4fa30851ad08
alsgur <qkralsgur721@gmail.com>
2022-09-06 23:59:37 +0900

    테스트 컨트롤러 작성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
 REPOSITORY=/home/ec2-user
+PROJECT_NAME=test-deploy
 
+cd $REPOSITORY/$PROJECT_NAME
 
-echo "> Build 파일 복사"
+echo "> Git pull"
 
-cp $REPOSITORY/*.jar $REPOSITORY/
+git pull
+
+echo "> 프로젝트 Build 시작"
+
+./gradlew build
+
+echo "> step1 디렉토리로 이동"
+
+cd $REPOSITORY
+
+echo "> Build 파일복사"
+
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl test-deploy | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
 
-echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+echo " 현재 구동중인 애플리케이션pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+        echo "> 현재구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep
 fi
 
-echo "> 새 어플리케이션 배포"
+echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep .jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.yml \
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+        -Dspring.config.location=classpath:/application.yml \
+        $REPOSITORY/$JAR_NAME 2>&1 &
