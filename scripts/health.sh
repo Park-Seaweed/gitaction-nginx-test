f61c9a49c4baa958fd82e3754187640b9cd831a0
alsgur <qkralsgur721@gmail.com>
2022-09-10 18:40:43 +0900

    제발 무중단 배포 성공 하면 내일 출근 안함

--- scripts/health.sh
+++ scripts/health.sh
@@ -6,7 +6,6 @@ source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
-HEALTH_CK=$(switch_proxy)
 
 
 echo "> Health Check Start!"
@@ -23,7 +22,7 @@ do
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
-      $HEALTH_CK   # switch.sh 실행
+      switch_proxy   # switch.sh 실행
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
--- src/main/java/com/project/instagramcloneteam5/controller/HealthController.java
+++ src/main/java/com/project/instagramcloneteam5/controller/HealthController.java
@@ -19,7 +19,7 @@ public class HealthController {
 
     @GetMapping("/health")
     public ResponseEntity<String> healthcheck(){
-        return ResponseEntity.ok("please");
+        return ResponseEntity.ok("die");
     }
 
 
