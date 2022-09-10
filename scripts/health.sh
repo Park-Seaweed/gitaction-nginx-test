8d8353142b279f04ec504a88a8bbbc28f6182940
alsgur <qkralsgur721@gmail.com>
2022-09-10 18:33:49 +0900

    제발 무중단 배포 성공 하면 내일 출근 안함

--- scripts/health.sh
+++ scripts/health.sh
@@ -17,12 +17,12 @@ for RETRY_COUNT in {1..10}  # for문 10번 돌기
 do
   RESPONSE=$(sudo curl -s http://127.0.0.1:${IDLE_PORT}/api/profile)
   UP_COUNT=$(echo ${RESPONSE} | grep "real" | wc -l)
-  HEALTH_CK=$(switch_proxy)
+
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
-      $HEALTH_CK   # switch.sh 실행
+      switch_proxy   # switch.sh 실행
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
