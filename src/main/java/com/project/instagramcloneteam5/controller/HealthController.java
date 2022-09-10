9717799200810a4f24c9455404c45c1fdddfba95
alsgur <qkralsgur721@gmail.com>
2022-09-10 19:01:32 +0900

    제발 무중단 배포 성공 하면 내일 출근 안함

--- src/main/java/com/project/instagramcloneteam5/controller/HealthController.java
+++ src/main/java/com/project/instagramcloneteam5/controller/HealthController.java
@@ -19,7 +19,7 @@ public class HealthController {
 
     @GetMapping("/health")
     public ResponseEntity<String> healthcheck(){
-        return ResponseEntity.ok("diiiiiiiiiiii");
+        return ResponseEntity.ok("Fucking");
     }
 
 
